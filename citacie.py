e757444be729e764321dd5056c78a746b231ae92
Martin Sucha <martin.sucha@fmph.uniba.sk>
2013-10-22 17:05:52 +0200

    Pridany zakladny layout postaveny nad HTML5 boilerplate

--- citacie.py
+++ citacie.py
@@ -1,11 +1,47 @@
+import re
+
 from flask import Flask
 app = Flask(__name__)
 
 from flask import render_template
+from flask import request
+
+from werkzeug.exceptions import BadRequest
+
+import wok
+
+data_sources = [wok.WOK()]
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/search-by-author')
+def search_by_author():
+  name = request.args.get('name', '')
+  surname = request.args.get('surname', '')
+  year = request.args.get('year', '')
+  
+  if len(name) == 0:
+    name = None
+  
+  if len(surname) == 0:
+    raise BadRequest()
+  
+  if len(year) > 0:
+    if not re.match(r'^\d*$', year):
+      raise BadRequest()
+  else:
+    year = None
+  
+  results = []
+  for data_source in data_sources:
+    with data_source as conn:
+      results.extend(conn.search_by_author(surname, name=name, year=year))
+  
+  return render_template('search-by-author.html',
+    search_name=name, search_surname=surname, search_year=year,
+    results=results)
 
 if __name__ == '__main__':
   import os
