ed71892c27c447b786beb2be45e6249c54220ce2
Martin Sucha <martin.sucha@fmph.uniba.sk>
2013-11-04 16:28:59 +0100

    Pridana dokumentacia k interface-u data source objektov

--- data_source.py
+++ data_source.py
@@ -6,6 +6,10 @@ class DataSource(object):
   
   @abstractmethod
   def connect(self):
+    """Vyrobi novy connection objekt pre dany data source, ak by nahodou bolo
+       treba rozne connection v roznych threadoch, malo by to vzdy vracat novu
+       (alebo nepouzivanu poolovanu) instanciu
+    """
     raise NotImplemented
   
   def __call__(self):
@@ -16,10 +20,16 @@ class DataSourceConnection(object):
   
   @abstractmethod
   def search_by_author(self, surname, name=None, year=None):
+    """Vrati iterable vracajuci zoznam publikacii pre dane meno a rok
+       - priezvisko hlada presne
+       - meno hlada ako prefix (t.j. T najde aj Tomas)
+       - ak je rok zadany, vracia len zaznamy pre dany rok, inak pre vsetky roky
+    """
     raise NotImplemented
   
   @abstractmethod
   def close(self):
+    """Uvolni zdroje pouzivane tymto objektom"""
     raise NotImplemented
   
   def __enter__(self):
